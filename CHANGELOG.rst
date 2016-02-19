a2e98e2d7d1d6bd62d4b6bf6a068a4c972f19ffa
Wesley Baugh <wesley@bwbaugh.com>
2016-02-18 23:03:06 -0800

    Support summing over non-nested JSON objects

---
+++ CHANGELOG.rst
@@ -0,0 +1,17 @@
+=========
+CHANGELOG
+=========
+
+All notable changes to this project will be documented in this file.
+This project adheres to `Semantic Versioning <http://semver.org/>`_.
+
+
+**********
+Unreleased
+**********
+
+Added
+=====
+
+* Minimum viable product (MVP) functionality.
+  Supports summing over non-nested JSON objects.
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.List
+import Data.Maybe
+
+import Data.Aeson
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Encoding as T
+import qualified Data.Text.Lazy.IO as T
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main =
+    T.interact $
+    newLine . T.decodeUtf8 . encode . sumObjects . map parse . T.lines
+  where
+    parse = fromMaybe (error "parse failed") . decode . T.encodeUtf8
+    newLine = T.unlines . (: [])
+
+sumObjects :: [Value] -> Value
+sumObjects = Object . foldl' go HM.empty
+  where
+    go acc (Object o) = HM.unionWith f acc o
+    go _ _ = error "expected Object"
+    f (Number new) (Number old) = Number (new + old)
+    f _ _ = error "expected Number. nested Objects currently unsupported."
